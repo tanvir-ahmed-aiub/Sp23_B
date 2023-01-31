07b19b384cbd76dec59dc7772ecdc7123ae77909
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-01-31 14:30:36 +0600

    Data Passing

---
+++ DataPassingandLayout/DataPassingandLayout/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataPassingandLayout.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Index";
+            return View();
+        }
+        public ActionResult MyProfile() {
+            //
+            //
+            //
+            //
+            ViewBag.Title = "Profile";
+            ViewBag.Name = "Sabbir";
+            ViewBag.Course = "Adv. .Net";
+            ViewBag.Section = "A";
+            ViewBag.Students = new string[] { "Shifat","Tahmid","Fahim","Israt","Nazia","Fatima"};
+            return View();
+        }
+        public ActionResult Settings() {
+            ViewBag.Title = "Settings";
+            return View();
+        }
+        public ActionResult Logout() {
+            return View();
+        }
+    }
+}
---
+++ DataPassingandLayout/DataPassingandLayout/Controllers/PagesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataPassingandLayout.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Home() {
+            return View();
+        }
+        public ActionResult About() {
+            return View();
+        }
+        public ActionResult Contact() {
+            return View();
+        }
+        public ActionResult Login() {
+            return View();
+        }
+        public ActionResult Signup() {
+            return View();
+        }
+        public ActionResult SignupSubmit()
+        {
+            return Redirect("https://portal.aiub.edu");
+        }
+        public ActionResult LoginSubmit() {
+            //if ok
+            TempData["Msg"] = "Login Successful";
+            return RedirectToAction("Index","Dashboard");
+        }
+    }
+}
