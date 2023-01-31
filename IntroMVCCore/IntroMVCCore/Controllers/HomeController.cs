7d026fe7c7b9fe0b7fa567d6e3262525f9e18e5b
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-01-31 12:03:22 +0600

    Intro

---
+++ IntroMVCCore/IntroMVCCore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using IntroMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IntroMVCCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MyPage() { 
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
