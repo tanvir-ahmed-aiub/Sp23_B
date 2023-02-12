1dec1aae045731e0893f41201edde19907f5db08
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-02-12 14:33:55 +0600

    Validation

--- FormSubmission/FormSubmission/Models/Student.cs
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -7,7 +7,7 @@ namespace FormSubmission.Models
 {
     public class Student
     {
-        public string Name { get; } // .Name
+        public string Name { get; set; } // .Name
         public string Id { get; set; }
         public double Cgpa { get; set; }
 
